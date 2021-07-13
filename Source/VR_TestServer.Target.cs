e250ab5ba88ae57ff38e984b8d6e9cfe84aaeb4c
wizboyd <wizboyd@gamersrus.ca>
2021-07-13 08:06:23 +0100

    commit

---
+++ Source/VR_TestServer.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+[SupportedPlatforms(UnrealPlatformClass.Server)]
+public class VR_TestServerTarget : TargetRules
+{
+    public VR_TestServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		BuildEnvironment = TargetBuildEnvironment.Shared;
+		ExtraModuleNames.Add("VR_Test");
+	}
+}
